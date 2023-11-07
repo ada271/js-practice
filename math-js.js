497652132f7c09d91d1ba7d6fd678f245561cf6c
ada <h.kaansolmaz@gmail.com>
2023-11-07 18:39:03 +0300

    first commit

---
+++ math-js.js
@@ -0,0 +1,54 @@
+// 2a
+let cost = 10 + 3 * 8 + 5 
+console.log(cost);
+
+// 2b 
+console.log(cost * 3 / 3);
+
+// 2c
+let c2 = 18.50 + 7.50 * 2
+console.log(c2);
+
+// 2d
+
+let tax = (1850 + 750 * 2) * 0.1 / 100
+console.log(tax)
+
+// 2e
+
+let tax20 = (1850 + 750 * 2) * 0.2/ 100
+console.log(tax20)
+
+//2f 
+
+let f2 = (2095 + 1899 + 799) / 100
+console.log(f2)
+
+//2g 
+
+let g2 = (2095 + 1899 + 799 + 499) / 100
+console.log(g2)
+
+// 2h
+
+let h2 = Math.round((2095 + 1899 + 799 + 499) * 0.1) / 100
+console.log(h2)
+
+// 2i
+
+
+let i2 = (2095 + 1899 + 799 + 499) / 100 + Math.round((2095 + 1899 + 799 + 499) * 0.1) / 100
+console.log(i2)
+
+// 2j
+let j2 = (25 * 9 / 5) + 32
+console.log(j2)
+
+//2k
+
+let k2 = (86 - 32) * 5 / 9
+console.log(k2)
+
+// 2n
+let n2 = (-5 * 9 / 5) + 32
+console.log(n2)
