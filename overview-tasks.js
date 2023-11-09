60759285e3e9638086b2fcd26c69aacace049247
ada <h.kaansolmaz@gmail.com>
2023-11-09 18:20:42 +0300

    overv- task1

---
+++ overview-tasks.js
@@ -0,0 +1,8 @@
+// task 1 avg of 3 numbers entered
+
+let a = parseInt(prompt("please enter a number"))
+let b = parseInt(prompt("please enter a number"))
+let c = parseInt(prompt("please enter a number"))
+
+let d = (a + b +c) / 3
+alert(`Average of the numbers entered ${d}`)
