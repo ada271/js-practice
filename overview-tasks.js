f8fde0b02d025a5aa1d1784d54ac282058f4e288
ada <h.kaansolmaz@gmail.com>
2023-11-09 18:51:53 +0300

    overw task3 grade calc

--- overview-tasks.js
+++ overview-tasks.js
@@ -9,16 +9,33 @@
 
 // task 2 find the type of triangle
 
-let s1 = parseInt(prompt("Enter the length of side one"))
-let s2 = parseInt(prompt("Enter the length of side two"))
-let s3 = parseInt(prompt("Enter the length of side three"))
+// let s1 = parseInt(prompt("Enter the length of side one"))
+// let s2 = parseInt(prompt("Enter the length of side two"))
+// let s3 = parseInt(prompt("Enter the length of side three"))
 
-if (s1 == s2 && s2 == s3 && s3 == s1) {
-    alert(`${s1},${s2},${s3} forms a equilateral triangle!`)
+// if (s1 == s2 && s2 == s3 && s3 == s1) {
+//     alert(`${s1},${s2},${s3} forms a equilateral triangle!`)
 
-}else if((s1 == s2 && s1 != s3) || (s1 == s3 && s1 != s2) || (s2 == s3 && s2 != s1)) {
-    alert(`${s1},${s2},${s3} forms a isosceles triangle!`)
+// }else if((s1 == s2 && s1 != s3) || (s1 == s3 && s1 != s2) || (s2 == s3 && s2 != s1)) {
+//     alert(`${s1},${s2},${s3} forms a isosceles triangle!`)
 
-}else{
-    alert(`${s1},${s2},${s3} forms a scalene triangle!`)
+// }else{
+//     alert(`${s1},${s2},${s3} forms a scalene triangle!`)
+// }
+
+
+//task3 grade calc
+
+let gradeMidterm = parseInt(prompt("Enter your midterm grade."))
+let gradeFinal = parseInt(prompt("Enter your final grade"))
+
+let totalGrade = (gradeMidterm * 40 / 100) + (gradeFinal * 60 / 100)
+
+if (totalGrade >= 50 && totalGrade <=100) {
+    alert("Congratulations, you've passed this course")
+
+} else if((gradeFinal < 0 || gradeFinal > 100) || (gradeMidterm < 0 || gradeMidterm > 100)) {
+    alert("Please enter valid grades!")
+} else {
+    alert("See you again next semester :D")
 }
