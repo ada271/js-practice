06cc1863d5aad7d6fe081ed34fb01ab0f2ff47ce
ada <h.kaansolmaz@gmail.com>
2023-11-16 18:30:55 +0300

    functions task 1-2-3

---
+++ practice with yt/functions1.js
@@ -0,0 +1,30 @@
+//task1
+
+// function greet() {
+//     console.log("Hello!");
+// }
+
+// greet();
+
+//task2
+
+// function greet(name) {
+//     console.log(` Hello ${name}`);
+// }
+
+// greet("ada");
+
+// greet("Bob");
+
+//task3 
+
+function greet(name) {
+    if (!name) {
+        console.log("Hello there!");
+    }else {
+        console.log(`Hello ${name}`);
+    }
+}
+
+greet("ada");
+greet()
